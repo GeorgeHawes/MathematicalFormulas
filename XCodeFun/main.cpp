657d06580c521aa4099d143ab49fca48fa94b7a5
lamalas600 <henryhawes1994@gmail.com>
2023-07-31 18:03:37 +0100

    Initial Commit

---
+++ XCodeFun/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  XCodeFun
+//
+//  Created by George Hawes on 31/07/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
